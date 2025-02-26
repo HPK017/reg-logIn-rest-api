827c02c43aa4fae575e6d2c665b46a488c4dd7c2
HPK017 <parelharsh0250@gmail.com>
2025-02-26 10:08:12 +0530

    registerUser method implemented

--- src/main/java/com/example/reg_login_rest_api/Controller/AuthController.java
+++ src/main/java/com/example/reg_login_rest_api/Controller/AuthController.java
@@ -1,5 +1,62 @@
 package com.example.reg_login_rest_api.Controller;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.reg_login_rest_api.DTO.SignUpDTO;
+import com.example.reg_login_rest_api.Model.Role;
+import com.example.reg_login_rest_api.Model.User;
+import com.example.reg_login_rest_api.Repository.RoleRepository;
+import com.example.reg_login_rest_api.Repository.UserRepository;
+
+@RestController
+@RequestMapping("api/auth")
 public class AuthController {
-    
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(SignUpDTO signUpDto) {
+
+        if (userRepository.existsByUsername(signUpDto.getUsername())) {
+            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signUpDto.getEmail())) {
+            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setName(signUpDto.getName());
+        user.setUsername(signUpDto.getUsername());
+        user.setEmail(signUpDto.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
+
+        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User Registered successfully", HttpStatus.OK);
+    }
+
 }
