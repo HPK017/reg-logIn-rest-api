10d6c51d337a5aac179e4d480c0cebf9996a1030
HPK017 <parelharsh0250@gmail.com>
2025-02-26 10:34:16 +0530

    changes to long to Long

--- src/main/java/com/example/reg_login_rest_api/Model/Role.java
+++ src/main/java/com/example/reg_login_rest_api/Model/Role.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(length = 60)
     private String name;
